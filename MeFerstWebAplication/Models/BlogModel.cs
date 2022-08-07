39cc98f428237632eb0cecebcdf8661d880a0b63
Сергей Пауль <loy4for@mail.ru>
2022-08-07 15:57:59 +0700

    Сдлелал вывод в блог генерируемый через list (под бд)

---
+++ MeFerstWebAplication/Models/BlogModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MeFerstWebAplication.Models
+{
+    public class BlogModel
+    {
+        public BlogModel()
+        {        }
+        public BlogModel(string? categori, string? text_Content, string? url_image, string? time)
+        {
+            Categori = categori;
+            Text_Content = text_Content;
+            Url_image = url_image;
+            Time = time;
+        }
+        public string? Id { get; set; }
+        public string? Categori { get; set; }
+        public string? Text_Content { get; set; }
+        public string? Url_image { get; set; }
+        public string? Time { get; set; }
+
+        
+
+    }
+}
